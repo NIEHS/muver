724f4cfe244205818727dded79862ad8a864c777
lavenderca <c.andrew.lavender@gmail.com>
2017-07-21 10:01:18 -0400

    Add check_reference_indices function

--- muver/reference.py
+++ muver/reference.py
@@ -12,6 +12,29 @@ def create_reference_indices(ref_fn):
     picard.create_sequence_dictionary(ref_fn)
 
 
+def check_reference_indices(ref_fn):
+    '''
+    For a given reference FASTA file, check for reference indices.
+    '''
+    # Bowtie2 and samtools suffixes
+    suffixes = [
+        '.1.bt2',
+        '.2.bt2',
+        '.3.bt2',
+        '.4.bt2',
+        '.rev.1.bt2',
+        '.rev.2.bt2',
+        '.fai',
+    ]
+    check_suffixes = \
+        [os.path.exists('{}{}'.format(ref_fn, suffix)) for suffix in suffixes]
+
+    picard_dict = '{}{}'.format(os.path.splitext(ref_fn)[0], '.dict')
+    check_suffixes.append(os.path.exists(picard_dict))
+
+    return all(check_suffixes)
+
+
 def read_chrom_sizes(reference_assembly_fn):
     '''
     Iterate through a FASTA file to find the length of each chromosome. If a
