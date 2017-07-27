8a31c7a555f7a0fbf5eb92fbe51ec3b548a11f24
lavenderca <c.andrew.lavender@gmail.com>
2017-07-27 16:06:23 -0400

    Refactor allelic_fraction. Now lean on numpy's histogram function

--- muver/allelic_fraction.py
+++ muver/allelic_fraction.py
@@ -1,5 +1,7 @@
 from collections import defaultdict
 import re
+import numpy
+import csv
 
 from wrappers.samtools import mpileup_iter
 
@@ -8,24 +10,14 @@ def get_allelic_fractions(bam_file, ref_fn, output_file):
     '''
     Read over a BAM file, output an allele fraction histogram.
     '''
-    fraction_histogram = defaultdict(int)
+    allele_fractions = []
 
     for line in mpileup_iter(bam_file, ref_fn):
 
-        present_alleles = set()
-
-        #  TODO: separate plus and minus tallies are unnecessary
-        plus_tally = defaultdict(int)
-        minus_tally = defaultdict(int)
-        deletion_count = 0  # For '*' bases
-        total = 0
-
-        previous = None
-        skip = None
+        allele_counts = defaultdict(int)
 
         line_split = line.strip().split()
-
-        reference, coverage = line_split[2:4]
+        reference_allele, coverage = line_split[2:4]
         if int(coverage) > 0:
             bases = line_split[4]
         else:
@@ -33,62 +25,39 @@ def get_allelic_fractions(bam_file, ref_fn, output_file):
 
         i = 0
         while i < len(bases):
-
-            if bases[i] == '.':
-                present_alleles.add(reference)
-                plus_tally[reference] += 1
-                total += 1
-            elif bases[i] == ',':
-                present_alleles.add(reference)
-                minus_tally[reference] += 1
-                total += 1
-
-            elif re.match('[ACGT]', bases[i]):
-                present_alleles.add(bases[i])
-                plus_tally[bases[i]] += 1
-                total += 1
-            elif re.match('[acgt]', bases[i]):
-                present_alleles.add(bases[i].upper())
-                minus_tally[bases[i]] += 1
-                total += 1
-
+            if re.match('[.,]', bases[i]):
+                allele_counts[reference_allele] += 1
+            elif re.match('[ACGTacgt]', bases[i]):
+                allele_counts[bases[i].upper()] += 1
             elif re.match('[+-]', bases[i]):
                 indel_type = bases[i]
                 i += 1
-
                 indel_length = int(bases[i])
                 i += 1
-
                 indel = indel_type + bases[i:i+indel_length].upper()
-                present_alleles.add(indel)
-                if re.match('[ACGT]', bases[i:i+indel_length]):
-                    plus_tally[indel] += 1
-                elif re.match('[acgt]', bases[i:i+indel_length]):
-                    minus_tally[indel] += 1
-                total += 1
+                allele_counts[indel] += 1
                 i += indel_length
-
             elif bases[i] == '^':
                 i += 1
             elif bases[i] == '*':
-                deletion_count += 1
-                total += 1
-
+                allele_counts[bases[i]] += 1
             i += 1
 
-        if total > 20:
-            for allele in present_alleles:
-                fraction = float(plus_tally[allele] + minus_tally[allele]) \
-                    / total
-                fraction_histogram[str(round(fraction, 2))] += 1
-            if deletion_count > 0:
-                fraction = float(deletion_count) / total
-                fraction_histogram[str(round(fraction, 2))] += 1
+        _sum = sum(allele_counts.values())
+        if _sum > 20:
+            for value in allele_counts.values():
+                allele_fractions.append(float(value) / _sum)
+
+    bins = [(0.01 * i) for i in range(101)]
+    histogram = numpy.histogram(allele_fractions, bins=bins, density=False)
 
     with open(output_file, 'w') as OUT:
-        for i in range(101):
-            display = str(round(float(i)/100, 2))
-            OUT.write('{}\t{}\n'.format(
-                display,
-                fraction_histogram[display],
-            ))
+        fieldnames = ['Bin start', 'Count']
+        writer = csv.DictWriter(OUT, fieldnames=fieldnames, delimiter='\t')
+
+        writer.writeheader()
+        for value, bin_start in zip(histogram[0], histogram[1]):
+            writer.writerow({
+                'Bin start': str(bin_start),
+                'Count': str(value),
+            })
